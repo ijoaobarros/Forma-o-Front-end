5470d6201b71d9bb34946bb46cdbccbcb8c2b6eb
João Paulo <joaopaulo_us@hotmail.com>
2024-06-11 18:35:36 -0300

    javascript

---
+++ 05_JAVASCRIPT/1-Intro/script/script.js
@@ -0,0 +1,24 @@
+console.log(typeof(13.15))
+
+console.log("Oi" + ", tudo bem?")
+
+// interpolação
+console.log(`A soma de 2 + 2 é: ${2 + 2}`)
+
+// boolean
+console.log(!false)
+
+// operador idêntico === e !==
+console.log(7 == '7')   //true
+console.log(7 === '7')  //false
+console.log(7 != '7')   //false
+console.log(7 !== '7')  //true
+
+
+/*
+    Operadores lógicos:
+    && - and
+    || - or
+    !  - not
+*/
+console.log('matheus' == true)
---
+++ 05_JAVASCRIPT/2-Estruturas/js/script.js
@@ -0,0 +1,22 @@
+let nome = 'João';
+
+console.log(nome)
+
+nome = 'João Paulo'
+console.log(nome)
+
+const idade = 26 //não pode ser alterada
+
+//formas diferentes que pode nomear uma variável
+let _underline = 'x'
+let $sifrao = 'x'
+
+// semelhante ao input no python:
+//const x = prompt('Digite um valor') 
+
+// objeto math
+let valor_maximo = Math.max(1, 9, 7, 15)
+console.log(valor_maximo)
+
+
+
