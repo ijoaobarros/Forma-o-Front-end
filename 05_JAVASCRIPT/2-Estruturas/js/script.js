46eceb9a6dba7000485486dcf1b5fd14b8c43dea
João Paulo <joaopaulo_us@hotmail.com>
2024-06-12 10:14:19 -0300

    javascript

--- 05_JAVASCRIPT/2-Estruturas/js/script.js
+++ 05_JAVASCRIPT/2-Estruturas/js/script.js
@@ -1,10 +1,5 @@
 let nome = 'João';
 
-console.log(nome)
-
-nome = 'João Paulo'
-console.log(nome)
-
 const idade = 26 //não pode ser alterada
 
 //formas diferentes que pode nomear uma variável
@@ -17,6 +12,62 @@ let $sifrao = 'x'
 // objeto math
 let valor_maximo = Math.max(1, 9, 7, 15)
 console.log(valor_maximo)
+console.log(Math.ceil(5.15))
+console.log(Math.floor(5.15))
+
+// erro e warn
+console.error('sou um erro')
+console.warn('sou um warn')
+
+// if-else
+const m = 10;
+if (m > 5) {
+    console.log('M é maior que 5')
+}
+
+const loggedin = false;
+if (loggedin) {
+    console.log('Está autenticado!');
+} else {
+    console.log('Não está autenticado');
+}
+
+const userName = 'Jose'
+const userAge = 31
+
+if(userName === 'José') {
+    console.log('Bem-vindo, José!')
+} else if (userName === 'Matheus' && userAge === 31) {
+    console.log('Olá, Matheus')
+} else {
+    console.log('Nenhuma condição aceita!')
+}
 
 
+// Estruturas de repetição
+let a = 2
+while (a) {
+    console.log(`Repetindo: ${a}`)
+    a--;
+}
 
+a = 0
+do {
+    console.log(`do-while: ${a}`)
+} while (a)    // executa pelo menos uma vez mesmo que a condição seja falsa
+
+
+for (let t = 0; t < 3; t++) {
+    console.log('Repetindo alguma coisa.');
+}
+
+
+//funções
+function fatorial(numero) {
+    if (numero === 1) {
+        return 1;
+    }
+    return numero*fatorial(numero-1);
+}
+
+console.log(fatorial(5))
